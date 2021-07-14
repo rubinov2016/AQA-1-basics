00050c99d8470616ee5f01a7aaba9dd820cd90c9
Ivan Rubinov <lrubinov@yandex.ru>
2021-07-14 17:48:58 +0300

    first commit

---
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -0,0 +1,11 @@
+package ru.netology.service;
+
+//TODO Подключите Github Actions
+
+public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
+}
