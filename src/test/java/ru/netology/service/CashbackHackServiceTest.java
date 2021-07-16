6c77727844bd78153f56e0714ba560eaabab2e75
I Rubinov <lrubinov@yandex.ru>
2021-07-16 13:36:35 +0300

    First init

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,21 +6,21 @@ import org.testng.annotations.Test;
 public class CashbackHackServiceTest {
 
     @Test
-    void shouldCalculateBonusLessBoundary() {
+    public void shouldCalculateBonusLessBoundary() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int expected = 800;
         int actual = cashbackHackService.remain(200);
         Assert.assertEquals(actual,expected);
     }
     @Test
-    void shouldCalculateBonusEqualBoundary() {
+    public void shouldCalculateBonusEqualBoundary() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int expected = 0;
         int actual = cashbackHackService.remain(1000);
         Assert.assertEquals(actual,expected);
     }
     @Test
-    void shouldCalculateBonusExceedBoundary() {
+    public void shouldCalculateBonusExceedBoundary() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int expected = 100;
         int actual = cashbackHackService.remain(2900);
